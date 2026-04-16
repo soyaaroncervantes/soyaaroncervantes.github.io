882ddf3f8edec97c5f88a6dbb1833ac919997933
Aaron Cervantes <soy.aaron.cervantes@gmail.com>
2026-04-15 22:47:05 -0600

    chore: update NavItem component
    - Minor updates to NavItem.tsx

--- src/features/theme/components/nav/NavItem.tsx
+++ src/features/theme/components/nav/NavItem.tsx
@@ -1,10 +1,14 @@
 import { M3eNavItem, type M3eNavItemElement } from '@m3e/react/nav-bar'
 import type { ComponentProps, PropsWithChildren } from 'react'
 import { useCallback, useEffect, useRef } from 'react'
+import { Theme } from '..'
 import { useNav } from './Nav'
 
-type Props = PropsWithChildren & ComponentProps<typeof M3eNavItem> & {}
-export const NavItem = ({ children, onChange, selected, ...props }: Props) => {
+type Props = PropsWithChildren &
+  ComponentProps<typeof M3eNavItem> & {
+    icon?: string
+  }
+export const NavItem = ({ children, onChange, selected, icon, ...props }: Props) => {
   const { onSelected, item } = useNav()
   const handlersRef = useRef({ onSelected, item })
   const m3eNavItemRef = useRef<M3eNavItemElement>(null)
@@ -24,6 +28,7 @@ export const NavItem = ({ children, onChange, selected, ...props }: Props) => {
 
   return (
     <M3eNavItem {...props} selected={isSelected} ref={m3eNavItemRef} onChange={onChangeHandler}>
+      {icon && <Theme.Icon slot="icon" name={icon} />}
       {children}
     </M3eNavItem>
   )
