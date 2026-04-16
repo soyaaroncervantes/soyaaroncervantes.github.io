22aeda082b4c03d0eb79c5dccd0a745dcc061467
Aaron Cervantes <soy.aaron.cervantes@gmail.com>
2026-04-16 00:00:16 -0600

    feat: add M3E Nav Rail CSS custom properties with theming support
    - Create nav.module.css with M3E CSS custom properties for nav-bar and nav-rail
    - Apply variables to .nav and .nav.rail selectors
    - Override --m3e-nav-bar-container-color and --m3e-nav-rail-compact-width
    - Apply background-color to host element for Shadow DOM compatibility
    - Update NavRail to use navRail class from nav.module.css
    - Remove unused theme.module.css

--- src/features/theme/components/nav/NavRail.tsx
+++ src/features/theme/components/nav/NavRail.tsx
@@ -1,12 +1,17 @@
 import { M3eNavRail } from '@m3e/react/nav-rail'
 import type { ComponentProps, PropsWithChildren } from 'react'
 import { useNav } from './Nav'
+import styles from './nav.module.css'
 
 type Props = PropsWithChildren & ComponentProps<typeof M3eNavRail> & {}
-export const NavRail = ({ children, ...props }: Props) => {
+export const NavRail = ({ children, className, ...props }: Props) => {
   const { id } = useNav()
   return (
-    <M3eNavRail {...props} id={id}>
+    <M3eNavRail
+      {...props}
+      id={id}
+      className={[styles.nav, styles.rail, className].filter(Boolean).join(' ')}
+    >
       {children}
     </M3eNavRail>
   )
