6fa42cdf2f4568869e108c2ef7abb642b87d17fb
Aaron Cervantes <soy.aaron.cervantes@gmail.com>
2026-04-22 20:50:19 -0600

    refactor: consolidate Icon and Svg components into icon/ directory
    - Remove duplicate Icon.tsx and Svg.tsx from components/ root
    - Remove outdated tests from src/test/features/theme/components/
    - Icon compound now lives exclusively in icon/Icon.tsx with Icon.Svg sub-component
    - Icon.Svg (ThemeIconSvg) is defined in icon/Svg.tsx
    - Import Icon directly: import { Icon } from '@/features/theme/components/icon/Icon'
    - Aligns with definitive compound component structure

--- src/features/theme/components/Icon.tsx
+++
@@ -1,8 +0,0 @@
-import { M3eIcon } from '@m3e/react/icon'
-import type { ComponentProps, PropsWithChildren } from 'react'
-
-export type ThemeIconProps = PropsWithChildren & ComponentProps<typeof M3eIcon> & {}
-
-export const ThemeIcon = ({ children, ...props }: ThemeIconProps) => {
-  return <M3eIcon {...props}>{children}</M3eIcon>
-}
