16465df3922ee66004ee3dde2810a3d48d64e513
Aaron Cervantes <soy.aaron.cervantes@gmail.com>
2026-04-22 20:24:56 -0600

    refactor(theme): remove Button and IconButton components
    - Remove Button.tsx and IconButton.tsx from components/
    - Remove corresponding test files
    - These components are no longer used in the theme namespace

--- src/features/theme/components/Button.tsx
+++
@@ -1,8 +0,0 @@
-import { M3eButton } from '@m3e/react/button'
-import type { ComponentProps, PropsWithChildren } from 'react'
-
-export type ThemeButtonProps = PropsWithChildren & ComponentProps<typeof M3eButton> & {}
-
-export const ThemeButton = ({ children, ...props }: ThemeButtonProps) => {
-  return <M3eButton {...props}>{children}</M3eButton>
-}
--- src/features/theme/components/IconButton.tsx
+++
@@ -1,8 +0,0 @@
-import { M3eIconButton } from '@m3e/react/icon-button'
-import type { ComponentProps, PropsWithChildren } from 'react'
-
-export type ThemeIconButtonProps = PropsWithChildren & ComponentProps<typeof M3eIconButton> & {}
-
-export const ThemeIconButton = ({ children, ...props }: ThemeIconButtonProps) => {
-  return <M3eIconButton {...props}>{children}</M3eIconButton>
-}
